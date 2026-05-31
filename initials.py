1cff731299bf0f91899b10e47fcd47f5f2fafe62
Josephine-45 <2425402235@live.gctu.edu.gh>
2026-05-30 23:19:50 -1200

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,29 @@
+#File: initials.py
+#Description: Print out my initials.
+#Assignment Number: 1
+#
+#Name: Josephine Adjoa Blay
+#STUDENT ID: 2425402235
+#Email:2425402235@live.gctu.edu.gh
+#Grader:Augustus Buckman
+#
+#On my honor, Josephine Adjoa Blay, this programming assignment is my own work
+#and I have not provided this code to any other student.
 
-def main():
-  pass #code goes here
 
-
-
+def main():  #write initials
+    print()
+    print("...JAB")
+    print()
+    print("...........JJJJ...........AAAAAA...........BBBBBBBB.........")
+    print("...........JJJJ.........AAAA..AAAA.........BBBBBBBBBB.......")
+    print("...........JJJJ........AAAA....AAAA........BBB......BBB.....")
+    print("...........JJJJ........AAA......AAA........BBBB....BBBB.....")
+    print("...........JJJJ........AA.AAAAAA.AA........BBBBBBBBBB.......")
+    print("...........JJJJ........AAA.AAAA.AAA........BBBBBBBBB........")
+    print("...........JJJJ........AAA......AAA........BBBB....BBBB.....")
+    print("...JJJJ....JJJJ........AAA......AAA........BBB......BBB.....")
+    print("...JJJJ...JJJJ....**...AAA......AAA...**...BBBBBBBBBB.....**")
+    print(".....JJJJJJJ......**...AAA......AAA...**...BBBBBBBB.......**")
 main()
+
